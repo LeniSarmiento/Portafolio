e11c35595fcf47714d4ef2bf2f67b0e9d688fb17
LeniSarmiento <leninmamanisarmiento.2001@gmail.com>
2024-08-16 09:14:56 -0500

    Cambio de a php

---
+++ Portafolio/index/login.php
@@ -0,0 +1,72 @@
+
+    <!DOCTYPE html>
+    <html lang="es">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lenin David - Desarrollador Web</title>
+        <link rel="stylesheet" href="estilos.css">
+    </head>
+    
+    <body>
+        <header>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Proyectos</a>
+                <a href="#">Contacto</a>
+                <button>Contrátame</button>
+            </nav>
+        </header>
+        <main>
+            <section class="intro">
+                <h1>Soy <span>Lenin David</span><br>Desarrollador Web</h1>
+                <p>Esto es un portafolio Web creado utilizando HTML y CSS.</p>
+                <button href="../doc/LeninDavidMamaniSarmiento1223.pdf">Descargar</button>
+                <button>Ver Proyectos</button>
+            </section>
+            <section class="about">
+                <img src="../../Portafolio/img/Perfil.png" alt="Lenin David">
+                <div>
+                    <h2>Acerca de mí</h2>
+                    <p>Soy un desarrollador web con experiencia en la creación de sitios web responsivos y funcionales. Mi objetivo es siempre entregar productos de alta calidad que cumplan con las expectativas del cliente y mejoren la experiencia del usuario.</p>
+                    <button>Más información</button>
+                </div>
+            </section>
+            <section class="skills">
+                <h2>Mis habilidades</h2>
+                <div class="skill">
+                    <h3>Responsive design</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, vero facilis aut libero est molestias atque fuga ab quidem excepturi magnam adipisci harum quam hic architecto accusamus veritatis esse. Repellendus?</p>
+                </div>
+                <div class="skill">
+                    <h3>Sistemas de diseño</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ad ab qui nulla quidem! Dignissimos cumque fugiat reprehenderit consectetur provident. Nulla repellendus ex cumque rerum quo, molestias exercitationem animi reprehenderit.</p>
+                </div>
+                <div class="skill">
+                    <h3>Cooperación</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio tempora dignissimos placeat quibusdam quis incidunt, earum dolor nemo atque, beatae at? Et, incidunt accusantium. Odio saepe adipisci sequi molestias tenetur?</p>
+                </div>
+            </section>
+            <section class="projects">
+                <h2>Algunos de mis proyectos</h2>
+                <div class="project">
+                    <img src="project1.jpg" alt="Proyecto 1">
+                    <p>Una pasión</p>
+                </div>
+                <div class="project">
+                    <img src="project2.jpg" alt="Proyecto 2">
+                </div>
+                <div class="project">
+                    <img src="project3.jpg" alt="Proyecto 3">
+                </div>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2023 Lenin David</p>
+            <p>Contacto: <a href="mailto:contacto@lenindavid.com">contacto@lenindavid.com</a></p>
+        </footer>
+    </body>
+    
+    </html>
+
